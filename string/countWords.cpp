b3c6fbb4d94b336c2449f24986790ea4dbf145d1
madeibao <2901429479@qq.com>
2022-08-06 11:44:36 +0800

    move directory2

--- string/countWords.cpp
+++ string/countWords.cpp
@@ -8,10 +8,9 @@ class Solution {
 public:
     int countSegments(string s) {
         int ans = 0;
-
-        //
+        // 最后的单词后面没有空格，添加一个空格
         s += ' ';
-        for (int i = 1; i < s.size(); i++)
+        for (int i = 1; i < s.size(); i++) 
             if (s[i] == ' ' && s[i - 1] != ' ') ans++;
         return ans;
     }
